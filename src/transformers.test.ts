49218752f743ac69f238759b463c627b657bf19e
Brian Gann <bkgann@gmail.com>
2020-04-20 23:14:27 -0500

    add tests

--- src/transformers.test.ts
+++ src/transformers.test.ts
@@ -1,5 +1,6 @@
 import { transformers } from './transformers';
 import { TimeSeries } from '../tests/__mocks__/grafana/app/core/time_series2';
+import TableModel from 'grafana/app/core/table_model';
 
 describe('Transformers', () => {
   const time = new Date().getTime();
@@ -36,5 +37,57 @@ describe('Transformers', () => {
   it('description should match', () => {
     const desc = transformers.timeseries_to_rows.description;
     expect(desc).toEqual('Time series to rows');
+  });
+  it('timeseries_to_rows should...', () => {
+    const result = transformers.timeseries_to_rows.getColumns();
+    expect(result).toEqual([]);
+    const data = [] as any;
+    const model = new TableModel();
+    transformers.timeseries_to_rows.transform(data, null, model);
+    expect(model.type).toEqual('table');
+  });
+  it('timeseries_to_columns should...', () => {
+    const result = transformers.timeseries_to_columns.getColumns();
+    expect(result).toEqual([]);
+    const data = [] as any;
+    const model = new TableModel();
+    transformers.timeseries_to_columns.transform(data, null, model);
+    expect(model.type).toEqual('table');
+  });
+  it('timeseries_aggregations', () => {
+    const result = transformers.timeseries_aggregations.getColumns();
+    expect(result.length).toEqual(6);
+    const data = [] as any;
+    const model = new TableModel();
+    model.columns = [];
+    const panel = {
+      columns: []
+    };
+    transformers.timeseries_aggregations.transform(data, panel, model);
+    expect(model.type).toEqual('table');
+  });
+  it('annotations', () => {
+    const result = transformers.annotations.getColumns();
+    expect(result).toEqual([]);
+    const data = [] as any;
+    const model = new TableModel();
+    transformers.annotations.transform(data, null, model);
+    expect(model.type).toEqual('table');
+  });
+  it('table', () => {
+    const result = transformers.table.getColumns();
+    expect(result).toEqual([]);
+    const data = [] as any;
+    const model = new TableModel();
+    transformers.table.transform(data, null, model);
+    expect(model.type).toEqual('table');
+  });
+  it('json', () => {
+    const result = transformers.json.getColumns();
+    expect(result).toEqual([]);
+    const data = [] as any;
+    const model = new TableModel();
+    transformers.table.transform(data, null, model);
+    expect(model.type).toEqual('table');
   });
 });
