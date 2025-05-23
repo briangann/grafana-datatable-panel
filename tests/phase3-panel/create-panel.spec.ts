306bf0ef7e4b6ab4ed21397f68a35fed9c1c5cdc
Brian Gann <briangann@users.noreply.github.com>
2025-05-23 16:38:13 -0400

    Prep release for v2.0.0 (#266)
    * fix license
    
    * update plugin json with correct links
    
    * missing comma
    
    * fix test

--- tests/phase3-panel/create-panel.spec.ts
+++ tests/phase3-panel/create-panel.spec.ts
@@ -2,6 +2,6 @@ import { expect, test } from '@grafana/plugin-e2e';
 
 test('data query should return datatable', async ({ panelEditPage }) => {
   await panelEditPage.datasource.set('gdev-testdata');
-  await panelEditPage.setVisualization('Datatable for Grafana');
+  await panelEditPage.setVisualization('Datatable Panel');
   await expect(panelEditPage.refreshPanel()).toBeOK();
 });
