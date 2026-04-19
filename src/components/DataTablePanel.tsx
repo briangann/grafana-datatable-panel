700782b4f079e916eb4b416dc067df67c783e472
Brian Gann <bkgann@gmail.com>
2026-04-19 03:31:11 -0400

    refactor(data): move buildSearchBarLayout to data/layout/ (refs #298)

--- src/components/DataTablePanel.tsx
+++ src/components/DataTablePanel.tsx
@@ -26,7 +26,7 @@ import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { DatatableOptions } from 'types';
 import { BuildColumnDefs, ConvertDataFrameToDataTableFormat } from 'data/dataHelpers';
 import { ApplyColumnWidthHints } from 'data/columns/columnWidthHints';
-import { buildSearchBarLayout } from 'data/buildSearchBarLayout';
+import { buildSearchBarLayout } from 'data/layout/buildSearchBarLayout';
 import { datatableThemedStyles } from './styles';
 import { GetDataTransformerID } from 'data/transformations';
 import { DTColumnType } from 'data/types';
