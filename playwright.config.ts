6a222f9f71565c7002aaf6391998a7bb959e25b6
Brian Gann <bkgann@gmail.com>
2026-06-25 21:57:21 -0400

    chore(e2e): add line reporter in CI for live test output

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,9 @@ export default defineConfig({
   /**
    * Reporter to use. See https://playwright.dev/docs/test-reporters
    */
-  reporter: [['html', { open: 'never' }]],
+  reporter: process.env.CI
+    ? [['line'], ['html', { open: 'never' }]]
+    : [['html', { open: 'never' }]],
 
   /**
    * Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions.
